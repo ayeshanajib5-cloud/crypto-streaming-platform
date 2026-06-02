ac3ee94b20cb9c4234ec4dbba7032b6cab4e64dc
Ayesha Najib <ayeshanajib5@gmail.com>
2026-06-02 06:51:50 +0500

    Updated Terraform outputs for EKS and ECR

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -9,3 +9,19 @@ output "cluster_endpoint" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "api_ecr_repository_url" {
+  value = aws_ecr_repository.api.repository_url
+}
+
+output "producer_ecr_repository_url" {
+  value = aws_ecr_repository.producer.repository_url
+}
+
+output "spark_ecr_repository_url" {
+  value = aws_ecr_repository.spark.repository_url
+}
+
+output "airflow_ecr_repository_url" {
+  value = aws_ecr_repository.airflow.repository_url
+}
